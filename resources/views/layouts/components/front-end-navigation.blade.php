4f7836684fa8d87417ea1c95c5edb08a1305ce7b
Tjoosten <Tjoosten3@gmail.com>
2017-06-22 02:43:56 +0200

    Update on navbar

--- resources/views/layouts/components/front-end-navigation.blade.php
+++ resources/views/layouts/components/front-end-navigation.blade.php
@@ -1,13 +1,5 @@
 <ul>
     <li><a href="{{ url('/') }}">Home</a></li>
-    <li><a href="{{ url('volunteer/new') }}">Word vrijwilliger</a></li>
-    <li><a href="mailto:info@activisme.be">Contact</a></li>
-
-    @if (auth()->check())
-        <li><a href="{{ url('backend') }}">Back-end</a></li>
-        <li><a href="{{ url('authencation/logout') }}">Uitloggen</a></li>
-    @else
-        <li><a href="{{ url('authencation/login') }}">Log-in</a></li>
-        <li><a href="{{ url('authencation/register') }}">Register</a></li>
-    @endif
+    <li><a href="http://www.vrijwilligers.activisme.be">Word vrijwilliger</a></li>
+    <li><a href="mailto:tom@activisme.be">Contact</a></li>
 </ul>
